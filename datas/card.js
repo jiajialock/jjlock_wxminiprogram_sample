c17c5cbddf2f1d1a9b77c4051785b645f5e832fd
jiajialock <jiajia_smart_lock@163.com>
2019-12-01 15:16:35 +0800

    去掉无用数据

--- datas/card.js
+++ datas/card.js
@@ -1,17 +1,8 @@
 const card = {
-  "lockId": "2c9204c56eb2203b016ebb768fd307fa",
-  "lockAlias": "Yunke lock 4F2802",
-  "keyId": "2c9204c56eb2203b016ebb7f5813081b",
-  "keyAlias": "测试",
+  "keyId": "",
+  "keyAlias": "test",
   "keyType": 2,
   "identifier": "00b05130d1",
-  "startDate": 0,
-  "endDate": 0,
-  "source": "2c9204c56ba24009016ba2c85336003c.2c9204c56eb2203b016ebb768fd307fb",
-  "comments": "",
-  "keyIndex": null,
-  "passwordType": 1,
-  "password": "38046431",
-  "createTime": "2019-11-30T08:48:35.000+0000"
+  "password": "54357819",
 }
 module.exports = card;
